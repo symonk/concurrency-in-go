57da71c1dc8a052191bae337815926b61795237c
symonk <jackofspaces@gmail.com>
2025-01-23 00:03:16 +0000

    adding `anonymous` function example of goroutines

--- goroutine/main.go
+++ goroutine/main.go
@@ -4,6 +4,15 @@ import "fmt"
 
 func main() {
 	go HelloWorld()
+
+	// Anonymous functions work too!
+	go func() {
+		fmt.Println("Hello World!")
+	}()
+
+	// Remember, goroutines are considered daemon threads
+	// This can exit without printing anything, depending on scheduling
+	// semantics!
 }
 
 func HelloWorld() {
